704333d1cc507ce1e4993a915b4ab81e487b97e9
Tomasz <urban776@gmail.com>
2019-11-13 14:04:51 +0100

    Change background image on AboutPage

--- dom.js
+++ dom.js
@@ -1,19 +1,21 @@
-// console.log(document.head);
-// console.log(document.body);
-// var name = 'Dupal';
-// console.log(document.all[10]);
 
-var workplacesIdEl = document.getElementById('workplaces');
-console.log(workplacesIdEl);
-
-var workplace = workplacesIdEl.getElementsByClassName('workplace');
-var workplaceHeader = workplace[0].getElementsByClassName('workplace-heading')
+// var workplaceHeader = workplace[0].getElementsByClassName('workplace-heading')
 // workplace[1].getElementsByClassName('workplace-heading')
 // function getWorkplaceHeaders(){
 //     for 
 // }
 
-var firstWorkplaceHeading = document.querySelector('.workplace:first-child');
+// var firstWorkplaceHeading = document.querySelector('.workplace:first-child');
 
-console.log(firstWorkplaceHeading);
+// console.log(firstWorkplaceHeading);
 // console.log(workplaceHeader.getElementByTagName('h2'));
+
+var homeSections = document.getElementsByClassName("home-section")
+
+changeBackgroundImg(0, "url(images/jatlo.jpg)");
+
+function changeBackgroundImg(elementIndex, bgImage){
+    homeSections[elementIndex].style.backgroundImage = bgImage;
+}
+
+console.log(homeSections[0].style.backgroundColor)
