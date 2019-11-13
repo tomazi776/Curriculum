93d5d551dc401642cc3824fe945bd9fe4cccdff7
Tomasz <urban776@gmail.com>
2019-11-13 18:44:21 +0100

    style section and button

--- scripts/dom.js
+++ scripts/dom.js
@@ -7,9 +7,6 @@
 
 // var firstWorkplaceHeading = document.querySelector('.workplace:first-child');
 
-// console.log(firstWorkplaceHeading);
-// console.log(workplaceHeader.getElementByTagName('h2'));
-
 var homeSections = document.getElementsByClassName("home-section")
 
 changeBackgroundImg(0, "url(images/jatlo.jpg)");
