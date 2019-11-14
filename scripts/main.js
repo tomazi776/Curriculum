bbc8ff444305ddbe0c825d7af80f6e6efebf9c12
Tomasz <urban776@gmail.com>
2019-11-14 11:50:41 +0100

    Add scripts folder and main.js file

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+const workplaces = Array.from(document.getElementsByClassName('timeline-contents'));
+console.log(workplaces.length)
+
+showOnScroll();
+
+
+function showOnScroll (yPos){
+    var divYPos = 200;
+    for (var i = 0; i < workplaces.length; i++){
+        var y = window.scrollY;
+        if (y >= divYPos){
+            if (i == 0){
+                workplaces[i].className = "timeline-contents timeline-now show"
+            }
+            else{
+                workplaces[i].className = "timeline-contents show"
+            }
+        } 
+        
+        else if (y < divYPos){
+            if (i == 0){
+                workplaces[i].className = "timeline-contents timeline-now hide"
+            }
+            else{
+                workplaces[i].className = "timeline-contents hide"
+            }
+        }
+        divYPos += 300;
+    }
+};
+
+    window.addEventListener("scroll", showOnScroll);
