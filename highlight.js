d437879d244bff65d5b963149a80dbee0b636e7e
Tomasz <urban776@gmail.com>
2020-01-18 19:33:28 +0100

    Fix text highligt

--- highlight.js
+++ highlight.js
@@ -1,7 +1,7 @@
 /** @type {HTMLElement} */
-
-var devText = document.querySelector('#dev');
 const photoText = document.getElementById('photo');
+
+const devText = document.getElementById('dev');
 
 const devBgContainer = document.getElementsByClassName('developer-portfolio')[0]
 const devContainerOverlay = document.getElementsByClassName('developer-portfolio-overlay')[0]
@@ -15,7 +15,11 @@ const photoOverlayPic = document.getElementsByClassName('photo-overlay-pic')[0]
 console.log(devText)
 console.log(photoText)
 console.log(devBgContainer)
+console.log(devContainerOverlay)
+console.log(devOverlayPic)
 console.log(photoBgContainer)
+console.log(photoContainerOverlay)
+console.log(photoOverlayPic)
 
 devBgContainer.addEventListener("mouseover", highlightDevText);
 devBgContainer.addEventListener("mouseout", fadeDevText);
@@ -25,63 +29,58 @@ photoBgContainer.addEventListener("mouseout", fadePhotoText);
 
 function highlightDevText(){
 
-    // if (element == "dev"){
-
-        devContainerOverlay.style.transition = "1s"
-        devContainerOverlay.style.backgroundColor = "black";
-        devContainerOverlay.style.opacity = 0.6
+        devContainerOverlay.style.transition = "0.7s"
+        devContainerOverlay.style.backgroundColor = "black"
+        devContainerOverlay.style.opacity = 0.8
 
         devOverlayPic.style.opacity = 1
+        devOverlayPic.style.transition = "1s"
         
-        devText.style.transition = "1s";
+        devText.style.transition = "1s"
         devText.style.opacity = 1
         devText.style.textShadow = "2px 2px 8px rgb(0,0,0,0.9)"
-    // }
-    // else {
-    // }
-
-}
-
-function highlightPhotoText(){
-
-        photoOverlayPic.style.opacity = 1
-
-        photoContainerOverlay.style.transition = "1s"
-        photoContainerOverlay.style.backgroundColor = "black";
-        photoContainerOverlay.style.opacity = 0.6
-        
-        photoText.style.transition = "1s";
-        photoText.style.opacity = 1
-        photoText.style.textShadow = "2px 2px 8px rgb(0,0,0,0.9)"
 }
 
 function fadeDevText(){
 
-    // if(element == "dev"){
-        devContainerOverlay.style.backgroundColor = "";
-        // devContainerOverlay.style.opacity = 0
+        devContainerOverlay.style.transition = "0.7s"
+        devContainerOverlay.style.backgroundColor = "black"
+        devContainerOverlay.style.opacity = 0.5
 
         devOverlayPic.style.opacity = 0.5
         devOverlayPic.style.transition = "1s"
 
+        devText.style.transition = "1s"
         devText.style.opacity = 0.5
         devText.style.textShadow = "0px 0px 0px rgba(0,0,0,.6)"
-    // }
-    // else{
-    // }
+}
 
+
+function highlightPhotoText(){
+
+    photoContainerOverlay.style.transition = "0.7s"
+    photoContainerOverlay.style.backgroundColor = "black"
+    photoContainerOverlay.style.opacity = 0.85
+
+    photoOverlayPic.style.opacity = 1
+    
+    photoText.style.transition = "1s"
+    photoText.style.opacity = 1
+    photoText.style.textShadow = "2px 2px 8px rgb(0,0,0,0.9)"
 }
 
 function fadePhotoText(){
 
+    photoContainerOverlay.style.transition = "0.7s"
+    photoContainerOverlay.style.backgroundColor = "black"
+    photoContainerOverlay.style.opacity = 0.6
 
-        photoContainerOverlay.style.backgroundColor = "";
-        // devContainerOverlay.style.opacity = 0
-        photoOverlayPic.style.opacity = 0.4
-        photoOverlayPic.style.transition = "1S"
+    photoOverlayPic.style.opacity = 0.5
+    photoOverlayPic.style.transition = "1s"
 
-        photoText.style.opacity = 0.4
-        photoText.style.textShadow = "0px 0px 0px rgba(0,0,0,.6)"
+    photoText.style.transition = "1s"
+    photoText.style.opacity = 0.5
+    photoText.style.textShadow = "0px 0px 0px rgba(0,0,0,.6)"
 }
 
 // devContainer.addEventListener("mouse")
